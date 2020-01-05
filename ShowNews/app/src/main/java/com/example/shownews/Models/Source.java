4689129d9d3ac66a37c8c2101fedfde42b6e0483
Shrey Agarwal <shrey.ag2806@gmail.com>
2020-01-05 12:13:04 +0530

    "Add Data Models of News"

---
+++ ShowNews/app/src/main/java/com/example/shownews/Models/Source.java
@@ -0,0 +1,30 @@
+package com.example.shownews.Models;
+
+import com.google.gson.annotations.SerializedName;
+
+
+
+class Source {
+
+    @SerializedName("id")
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @SerializedName("name")
+    private String name;
+}
